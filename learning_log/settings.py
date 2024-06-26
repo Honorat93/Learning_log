0212c038f3d2d4ed1efdb45ca897e91d1fcdfb43
Honorat93 <95083527+Honorat93@users.noreply.github.com>
2024-06-26 03:37:49 +0200

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -152,5 +152,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+CSRF_TRUSTED_ORIGINS = [
+    'web-production-b4ce.up.railway.app',  
+  
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
